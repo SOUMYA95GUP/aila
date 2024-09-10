dfef5236db29e259895bb6e534951aca04869cd7
Pranjan <1996prgupta@gmail.com>
2024-09-11 01:51:47 +0530

    leave application and leave balance

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 from pydantic import BaseModel
-from app.api.v1.endpoints import health,auth,secure_endpoint,query,leave
+from app.api.v1.endpoints import health,auth,secure_endpoint,query,leave,user
 from app.core import bedrock_agent_client
+from app.core.database import db_manager
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -16,6 +18,7 @@ app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(secure_endpoint.router, prefix="/secure", tags=["secure"])
 app.include_router(query.router)
 app.include_router(leave.router)
+app.include_router(user.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/static/{path}")
@@ -33,6 +36,7 @@ async def startup_event():
         print("Bedrock client successfully initialized.")
     else:
         print("Bedrock client initialization failed.")
+    db_manager.Base.metadata.create_all(bind=db_manager.engine)
 
 @app.get("/", response_class=HTMLResponse)
 async def get_ui(request: Request):
--- app/schema/leave.py
+++ app/schema/leave.py
@@ -4,25 +4,34 @@ from datetime import date
 from typing import Optional
 
 class LeaveRequest(BaseModel):
-    employee_id: str
-    start_date: date
+    user_name: str
+    start_date: str
     end_date: Optional[str] = None
     leave_type: str
 
 class GetLeaveRequest(BaseModel):
-    leave_id: str
-    start_date: Optional[date] = None
-    end_date: Optional[date] = None
+    user_name: str
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
     leave_type: Optional[str] = None
 
 class UpdateLeaveRequest(BaseModel):
-    leave_id: str
-    start_date: Optional[date] = None
-    end_date: Optional[date] = None
+    user_name: str
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
     leave_type: Optional[str] = None
 
 class DeleteLeaveRequest(BaseModel):
-    leave_id: str
-    start_date: Optional[date] = None
-    end_date: Optional[date] = None
+    user_name: str
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
     leave_type: Optional[str] = None
+
+class CheckLeaveRequest(BaseModel):
+    user_name: str
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
+    leave_type: Optional[str] = None
+
+
+
